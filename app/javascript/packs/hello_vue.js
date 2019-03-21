21cc02f97ee51f393a93b361e05c10129394be05
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-03-21 00:41:33 -0700

    pt 1 complete

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,82 +1,76 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %>
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
 import Vue from 'vue/dist/vue.esm'
-import TurbolinksAdapter from 'vue-turbolinks' // node module he made??
-import VueResource from 'vue-resource' // a way to send ajax that is a bit easier...
+import TurbolinksAdapter from 'vue-turbolinks' // some node module chris go rails made
+import VueResource from 'vue-resource' // so that we have way to send ajax a bit easier
 
-Vue.use(VueResource) //which will initialize all of that... // yarn add vue-turbolinks vue-resource
+Vue.use(VueResource) // which initilaizes all that
 Vue.use(TurbolinksAdapter) // https://stackoverflow.com/questions/46239040/error-vue-js-cannot-read-property-props-of-undefined
+
+// then go to terminal and go yarn add vue-turbolinks vue-resource
+// chris already added all these; see package.json <=> Gemfile
+
 document.addEventListener('turbolinks:load', () => {
-  // what the heck??
+
   Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  // we will have access to that csrf token that shows up on every page and we'll be able to submit across as part of the ajax request automatically
+  // wtf does that mean???
+  var element = document.getElementById("team-form")
 
-  var element = document.getElementById("team-form") // get the team form
-  if (element != null) { // set up vue only if on team-form
+  // if #team-form exists, invoke Vue microApp
+  if (element != null) {
 
-    var id = element.dataset.id
-    var team = JSON.parse(element.dataset.team)
-    var players_attributes = JSON.parse(element.dataset.playersAttributes)
-    players_attributes.forEach(function(player) { player._destroy = null })
-    team.players_attributes = players_attributes
+    var team = JSON.parse(element.dataset.team); // grab the team json attribute and parse it out
+      // data-team = {id: null, name: null}
+    var players_attributes = JSON.parse(element.dataset.playersAttributes) // camel case to access properly, weird quirk
+      // data-players-attributes = []
+    players_attributes.forEach(function(player) { player._destroy = null }) // so we can set up automatically to have _destroy when we have edit form to be able to monitor with vue,
+
+    // low key a clutch move in mirroring rails has_many/nested_attributes relations
+    team.players_attributes = players_attributes;
 
     var app = new Vue({
       el: element,
       data: function() {
-        return { id: id, team: team }
+        return { team: team }
       },
       methods: {
         addPlayer: function() {
-          this.team.players_attributes.push({
+          debugger;
+          // add another div in the same spirit as a new AR instance
+          team.players_attributes.push({
             id: null,
             name: "",
-            //position: "",
-            _destroy: null
+            // position: ""
+            _destroy: null // keep in sync with other destroy methods
           })
         },
 
         removePlayer: function(index) {
-          var player = this.team.players_attributes[index]
-
-          if (player.id == null) {
-            this.team.players_attributes.splice(index, 1)
-          } else {
-            this.team.players_attributes[index]._destroy = "1"
-          }
-        },
-
-        undoRemove: function(index) {
-          this.team.players_attributes[index]._destroy = null
-        },
-
-        saveTeam: function() {
-          // Create a new team
-          if (this.id == null) {
-            this.$http.post('/teams', { team: this.team }).then(response => {
-              Turbolinks.visit(`/teams/${response.body.id}`)
-            }, response => {
-              console.log(response)
-            })
-
-          // Edit an existing team
-          } else {
-            this.$http.put(`/teams/${this.id}`, { team: this.team }).then(response => {
-              Turbolinks.visit(`/teams/${response.body.id}`)
-            }, response => {
-              console.log(response)
-            })
-          }
-        },
-
-        existingTeam: function() {
-          return this.team.id != null
+          debugger;
+          // why do we need this here when we didn't when we added a player??
+          // theory: the remove button is nested in some for-loop...so we must jump out of the scope by doing 'this' (Vue) and going down to the team
+          this.team.players_attributes.splice(index, 1);
         }
-
       }
     })
-
   }
 })
+
+// define a mixin object
+// didn't need Turbolinks it turns out
+  // var myMixin = {
+  //   created: function () {
+  //     this.hello()
+  //   },
+  //   methods: {
+  //     hello: function () {
+  //       console.log('hello from mixin!')
+  //     }
+  //   }
+  // }
+  //
+  // // define a component that uses this mixin
+  // var Component = Vue.extend({
+  //   mixins: [myMixin]
+  // })
+  //
+  // var component = new Component() // => "hello from mixin!"
